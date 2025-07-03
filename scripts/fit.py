c49645df799065420b855b708a726cc6808d0963
AnRu32 <6182993@gmail.com>
2025-07-03 16:01:48 +0000

    Применение OneHotEncoder и LogisticRegression, новые параметры

--- scripts/fit.py
+++ scripts/fit.py
@@ -4,56 +4,64 @@ import os
 import joblib
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
-from category_encoders import CatBoostEncoder
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from catboost import CatBoostClassifier
+from sklearn.linear_model import LogisticRegression
 
-# Обучение модели
 def fit_model():
-    # Загрузка гиперпараметров из файла params.yaml
+    # Загрузка гиперпараметров
     with open('params.yaml', 'r') as fd:
         params = yaml.safe_load(fd)
 
     # Загрузка данных
     data = pd.read_csv('data/initial_data.csv')
+    target = 'target'
+    X = data.drop(columns=target)
+    y = data[target]
 
-    # Подготовка данных
-    target = 'target' # Предполагаем, что ваш целевой столбец называется "target"
-    X = data.drop(target, axis=1)  # Выделение признаков
-    y = data[target]  # Целевая переменная
-
-    cat_features = data.select_dtypes(include='object')
+    # Категориальные и числовые признаки
+    cat_features = X.select_dtypes(include='object')
     potential_binary_features = cat_features.nunique() == 2
 
+    # Разделение категориальных признаков
     binary_cat_features = cat_features[potential_binary_features[potential_binary_features].index]
     other_cat_features = cat_features[potential_binary_features[~potential_binary_features].index]
-    num_features = data.select_dtypes(['float'])
+    num_features = X.select_dtypes(include=['int', 'float'])
 
+    # Предобработка
     preprocessor = ColumnTransformer(
-        [
-            ('binary', OneHotEncoder(drop='if_binary'), binary_cat_features.columns.tolist()),
-            ('cat', CatBoostEncoder(return_df=False), other_cat_features.columns.tolist()),
-            ('num', StandardScaler(), num_features.columns.tolist())
+        transformers=[
+            ('binary', OneHotEncoder(drop='if_binary', handle_unknown='ignore', sparse_output=False),
+             binary_cat_features.columns.tolist()),
+            ('cat', OneHotEncoder(handle_unknown='ignore', sparse_output=False),
+             other_cat_features.columns.tolist()),
+            ('num', StandardScaler(), num_features.columns.tolist()),
         ],
         remainder='drop',
         verbose_feature_names_out=False
     )
 
-    # Конфигурация модели
-    model = CatBoostClassifier(auto_class_weights='Balanced')  # Предполагается, что гиперпараметры модели хранятся под ключом 'model'
+    # Модель
+    # model = CatBoostClassifier(auto_class_weights='Balanced', verbose=0)
+    # model = LogisticRegression(C=1.0, penalty='l2', solver='liblinear')
+    model_params = params.get("model", {})
+    model = LogisticRegression(
+        C=model_params.get("C", 1.0),
+        penalty=model_params.get("penalty", "l2"),
+        solver=model_params.get("solver", "liblinear")
+)
 
-    # Создание и обучение пайплайна
+    # Pipeline
     pipeline = Pipeline(steps=[
-            ('preprocessor', preprocessor),
-            ('model', model)
-     ])
-    
+        ('preprocessor', preprocessor),
+        ('model', model)
+    ])
+
     pipeline.fit(X, y)
 
-    # Сохранение обученной модели
-    os.makedirs('models', exist_ok=True) # создание директории, если её ещё нет
-    model_path = 'models/fitted_model.pkl' 
-    with open(model_path, 'wb') as fd:
+    # Сохранение модели
+    os.makedirs('models', exist_ok=True)
+    with open('models/fitted_model.pkl', 'wb') as fd:
         joblib.dump(pipeline, fd)
 
 if __name__ == '__main__':
